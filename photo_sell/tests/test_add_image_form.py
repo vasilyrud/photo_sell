b629d137e4321a5f742b33752a2e5e20a9a4df78
Vasily Rudchenko <vasilyrud@gmail.com>
2019-10-15 18:29:04 -0400

    Add url parsing tests

--- photo_sell/tests/test_add_image_form.py
+++ photo_sell/tests/test_add_image_form.py
@@ -2,7 +2,67 @@ import pytest
 
 from photo_sell.routes.add_image_form import AddImageForm
 
-def test_empty_db(client):
+VALID_SUBURLS = [
+    'drive.google.com/file/d/',
+    'drive.google.com/open?id=',
+]
 
+VALID_DRIVE_ID = '0B4Edc2SFos9ANm1yU2Q0YVhEZ2c'
+
+def test_parse_drive_url_valid(client):
     form = AddImageForm()
-    assert 1 == 1
+
+    valid_urls = [
+        'https://' + VALID_SUBURLS[0] + VALID_DRIVE_ID + '/view?usp=sharing',
+        VALID_SUBURLS[0] + VALID_DRIVE_ID + 'cd',
+    ] + [
+        suburl + VALID_DRIVE_ID for suburl in VALID_SUBURLS
+    ]
+
+    for valid_url in valid_urls:
+        assert form._parse_drive_url(valid_url) == VALID_DRIVE_ID
+
+def _assert_invalid_urls(form, invalid_urls):
+    for invalid_url in invalid_urls:
+        assert form._parse_drive_url(invalid_url) == None
+
+def test_parse_drive_url_invalid_length(client):
+    form = AddImageForm()
+
+    invalid_urls = [
+        VALID_SUBURLS[0] + VALID_DRIVE_ID[:-1],
+        VALID_SUBURLS[0],
+    ]
+
+    _assert_invalid_urls(form, invalid_urls)
+
+def test_parse_drive_url_invalid_prefix(client):
+    form = AddImageForm()
+
+    invalid_urls = [
+        'drive.google.com' + VALID_DRIVE_ID,
+        VALID_SUBURLS[0] + '/' + VALID_DRIVE_ID,
+        VALID_DRIVE_ID,
+    ]
+
+    _assert_invalid_urls(form, invalid_urls)
+
+def test_parse_drive_url_invalid_alnum(client):
+    form = AddImageForm()
+
+    invalid_urls = [
+        VALID_SUBURLS[0] + '=' + VALID_DRIVE_ID[1:],
+        VALID_SUBURLS[0] + VALID_DRIVE_ID[:-1] + '=',
+    ]
+
+    _assert_invalid_urls(form, invalid_urls)
+
+def test_parse_drive_url_exists(client):
+    form = AddImageForm()
+
+    assert form._drive_image_exists(VALID_DRIVE_ID)
+
+def test_parse_drive_url_does_not_exist(client):
+    form = AddImageForm()
+
+    assert not form._drive_image_exists(28 * 'a')
